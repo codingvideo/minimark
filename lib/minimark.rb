15c6b758de9ee055f18694bef527d67ccb8585d4
Andy <andy@Andys-MacBook-Pro.local>
2020-02-29 03:14:35 -0500

    refactor

--- lib/minimark.rb
+++ lib/minimark.rb
@@ -1,4 +1,5 @@
 module MiniMark
+  
   class Line
 
     attr :line_type
@@ -6,127 +7,101 @@ module MiniMark
     def initialize(str, scope=nil)
       @scope = scope
 
-      if @scope == :code  && str.strip != '```'
+      if(@scope == :code && Util.not_code_scope_close?(str))
         @str = str.rstrip + ' ' # rstrip only, retain left space
       else
         @str = str.strip 
       end
 
-      if(heading?)
-        @line_type = :heading
-      elsif(section?)
-        @line_type = :section
-      elsif(code?)
-        @line_type = :code
-      elsif(codeopen?)
-        @line_type = :codeopen
-      elsif(codeclose?)
-        @line_type = :codeclose
-      elsif(hint?)
-        @line_type = :hint
-      elsif(blank?)
-        @line_type = :blank
-      elsif(html?)
-        @line_type = :html
-      elsif(gonext?)
-        @line_type = :gonext
-      elsif(goback?)
-        @line_type = :goback
-      elsif(template?)
-        @line_type = :template
-      else
-        @line_type = :paragraph
+      if(heading?)     ;@line_type = :heading
+      elsif(section?)  ;@line_type = :section
+      elsif(code?)     ;@line_type = :code
+      elsif(codeopen?) ;@line_type = :codeopen
+      elsif(codeclose?);@line_type = :codeclose
+      elsif(hint?)     ;@line_type = :hint
+      elsif(blank?)    ;@line_type = :blank
+      elsif(html?)     ;@line_type = :html
+      elsif(gonext?)   ;@line_type = :gonext
+      elsif(goback?)   ;@line_type = :goback
+      elsif(template?) ;@line_type = :template
+      else             ;@line_type = :paragraph
       end
     end
 
-    def heading?
-      @str[0]=="#" && @str[1]!= "#"
-    end
-
-    def section?      
-      @str[0]=="#" && @str[1]=="#" && @str[2]!="#"
-    end
-
-    def code?
-      @scope == :code && @str != '```'
-    end
-
-    def codeopen?
-      str = @str.strip
-      str.match(/^```.+/) != nil
-    end
-
-    def codeclose?
-      @scope == :code && @str == '```'
-    end
-
-    def hint?
-      @str[0] == '|'
-    end
-
-    def blank?
-      @str == ''
-    end
-
-    def html?
-      @str[0] == '<' && @str[-1] == '>'
-    end
-
-    def gonext?
-      @str[0] == '-' && @str[1] == '>'
-    end
-
-    def goback?
-      @str[0] == '<' && @str[1] == '-'
-    end
-
-    def template?
-      @str[0] == '[' && @str[-1] == ']'
-    end
+    def heading?  ;@str[0]=="#" && @str[1]!= "#" ;end
+    def section?  ;@str[0]=="#" && @str[1]=="#" && @str[2]!="#" ;end
+    def code?     ;@scope == :code && @str != '```' ;end
+    def codeopen? ;@str.match(/^```.+/) != nil ;end
+    def codeclose?;@scope == :code && @str == '```' ;end
+    def hint?     ;@str[0] == '|' ;end
+    def blank?    ;@str == '' ;end
+    def html?     ;@str[0] == '<' && @str[-1] == '>' ;end
+    def gonext?   ;@str[0] == '-' && @str[1] == '>'  ;end
+    def goback?   ;@str[0] == '<' && @str[1] == '-'  ;end
+    def template? ;@str[0] == '[' && @str[-1] == ']' ;end
 
     def to_s
-      if(@line_type == :heading)
+
+      if(:heading == @line_type)
         str = @str.sub(/^#/, '').strip
         str =  str.sub('[', '<span class="boxed">')
         str =  str.sub(']', '</span>')
         return '<h1>' + str + '</h1>'
-      elsif(@line_type == :section)
+
+      elsif(:section == @line_type)
         str = @str.sub(/^##/, '').strip
         return '<h2>' + str + '</h2>'
-      elsif(@line_type == :code)
-        return replace_brackets(/___/, 'light')
-      elsif(@line_type == :codeopen)
+
+      elsif(:code == @line_type)
+        return Util.replace_brackets(@str, /___/, 'light')
+
+      elsif(:codeopen == @line_type)
         lang = @str.sub('```','').strip
         if(lang=='cmd')
           return '<pre class="cmd">'
         else
           return '<pre class="prettyprint lang-'+lang+'">'
         end
-      elsif(@line_type == :codeclose)
+
+      elsif(:codeclose == @line_type)
         return '</pre>'
-      elsif(@line_type == :hint)
+
+      elsif(:hint == @line_type)
         str = @str.sub(/^\|/, '').strip
         return '<p class="hint">' + str + '</p>'
-      elsif(@line_type == :html)
+
+      elsif(:html == @line_type)
         return @str
-      elsif(@line_type == :gonext)
-        return go_link(@str, 'next')
-      elsif(@line_type == :goback)
-        return go_link(@str, 'back')
-      elsif(@line_type == :template)
+
+      elsif(:gonext == @line_type)
+        return Util.go_link(@str, 'next')
+
+      elsif(:goback == @line_type)
+        return Util.go_link(@str, 'back')
+
+      elsif(:template == @line_type)
         template_path = @str.gsub(/\[|\]/, '').strip
         return File.read(template_path)
-      elsif(@line_type == :paragraph)
-        str = replace_brackets(/`/, 'mono')
+
+      elsif(:blank == @line_type)
+        return ""
+
+      else # :paragraph
+        str = Util.replace_brackets(@str, /`/, 'mono')
         str = str.sub(/^\^\s/, '&uarr; ')
         str = str.sub(/^v\s/, '&darr; ')
         return '<p>' + str + '</p>'
-      else # @line_type == :blank
-        return ""
       end
+    end#def
+  end #class
+
+  class Util
+
+    def self.not_code_scope_close?(str)
+      str.strip != '```'
     end
 
-    def go_link(str, _class)
+    def self.go_link(str, _class)
       parts = str.split(/\(|\)/)
       '<div class="go-box '+_class+'">' +
         '<a class="go '+_class+'" href="'+parts[1].strip+'">' +
@@ -137,30 +112,30 @@ module MiniMark
       '</div>'
     end
 
-    def replace_brackets(bracket, span_class)
-      bracket_count = @str.scan(bracket).size
+    def self.replace_brackets(str, bracket, span_class)
+      bracket_count = str.scan(bracket).size
       highlighted = bracket_count > 0 && bracket_count % 2 == 0
       if(highlighted)
-        segments = (@str+' ').split(bracket)
-        str = ''
+        segments = (str+' ').split(bracket)
+        out = ''
         bit = true
         segments.each_with_index do |s, i|
-          str += s
+          out += s
           if(i != segments.size-1) # not last
             if(bit) # open tag
-              str += '<span class="'+span_class+'">' 
+              out += '<span class="'+span_class+'">' 
             else # close tag
-              str += '</span>'
+              out += '</span>'
             end
             bit = !bit # flip the bit
           end
         end
-        return str
+        return out
       else
-        return @str
+        return str
       end
     end
-  end #class
+  end#class
 
   class Parser
     def initialize(filename)
@@ -169,7 +144,8 @@ module MiniMark
     end
 
     def parse
-      return @out.join("\n") if(@out != nil)
+      
+      return @out.join("\n") if(@out != nil) # caching
       
       file = File.open(@filename)
       lines = file.readlines
