3187765e5ccd9361e49d54c578249266e8f1947f
Andy <andy@Andys-MacBook-Pro.local>
2020-03-09 04:27:50 -0400

    update demo

--- demo/main.rb
+++ demo/main.rb
@@ -1,5 +1,6 @@
-require 'minimark'
+require 'mini_mark/parser'
+require 'mini_mark/custom_blog_line'
 
-parser = MiniMark::Parser.new('test.md')
+parser = MiniMark::Parser.new('test.md', MiniMark::CustomBlogLine)
 html = parser.parse
 File.write('test.html', '<html><body>'+html+'</body></html>')
